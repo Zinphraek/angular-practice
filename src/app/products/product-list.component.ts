482fa9a61629e0512c6a2508732c380bd97cdab5
zinphraek <zinphraek@gmail.com>
2022-11-28 14:42:29 -0500

    Explored Services and dependency.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -8,94 +9,45 @@ import { IProduct } from "./product";
 })
 export class ProductListComponent implements OnInit{
 
+  constructor(private productService: ProductService) {}
+  
+
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+
+  private _listFilter: string= '';
+  get listFilter(): string {
+      return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+      this._listFilter = value;
+      console.log(this._listFilter)
+      this.filteredProducts = this.performFilter(value);
+  }
+
+  products: IProduct[] = [];
+  filteredProducts: IProduct[] = [];
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+  }
+
+  toggleImage(): void{
+  this.showImage = !this.showImage;
+  }
+
   onRatingClicked(message: string): void {
-    this.pageTitle = 'Product List: ' + message;
-}
+      this.pageTitle = 'Product List: ' + message;
+  }
 
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-
-    private _listFilter: string= '';
-    get listFilter(): string {
-        return this._listFilter;
-    }
-
-    set listFilter(value: string) {
-        this._listFilter = value;
-        console.log(this._listFilter)
-        this.filteredProducts = this.performFilter(value);
-    }
-
-    filteredProducts: IProduct[] = [];
-
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2021",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2021",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2021",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": "assets/images/saw.png"
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2020",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": "assets/images/xbox-controller.png"
-        }
-      ]
-
-      toggleImage(): void{
-        this.showImage = !this.showImage;
-      }
-
-      ngOnInit(): void {
-        this._listFilter = 'cart';
-        console.log("Method not implemented.");
-    }
-
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) =>
-        product.productName.toLocaleLowerCase().includes(filterBy));
-    }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().includes(filterBy));
+  }
     
 }
